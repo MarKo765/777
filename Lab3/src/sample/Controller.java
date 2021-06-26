3a3e5d50ecaf1cd3ea46d3d83c95ff68b20c3760
MarKo765 <65297156+MarKo765@users.noreply.github.com>
2021-06-26 18:16:14 +0200

    Add files via upload

---
+++ Lab3/src/sample/Controller.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.Scanner;
+
+public class Controller {
+
+    public Button btnMain;
+    public TextField txtLicznik;
+
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr.:" + ++clickCounter);
+        if(clickCounter >=0){
+            txtLicznik.setText("Ilosc klikow:    " + clickCounter); }
+        if (clickCounter == 1){
+            btnMain.setText("Jeszcze raz");}
+
+
+    }
+}
